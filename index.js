ef9a3798dfbe17d992ca740bcc5d4fdcf8482482
Philipp Minges <fjnalta@gmail.com>
2021-10-27 19:24:50 +0200

    testing

--- index.js
+++ index.js
@@ -1,33 +1,66 @@
 const Apify = require('apify');
+
 const { log } = Apify.utils;
 
+const config = require('./config');
+
 Apify.main(async () => {
-    // Get the username and password inputs
-    //const input = await Apify.getValue('INPUT');
-
     const browser = await Apify.launchPuppeteer();
-    const page = await browser.newPage();
-    await page.goto('https://www.lehrerbuero.de/anmelden.html');
-    await page.waitForNetworkIdle();
 
-    // Accept cookies
-    await page.click('#uc-btn-accept-banner');
+    // const page = await browser.newPage();
+    //
+    // await page.goto('https://www.lehrerbuero.de/anmelden.html');
+    // await page.waitForNetworkIdle();
+    //
+    // // Accept cookies
+    // await page.click('#uc-btn-accept-banner');
+    //
+    // // Login
+    // await page.type('#user', config.username);
+    // await page.type('#pass', config.password);
+    // await page.click('button[name="submit"]');
+    //
+    // await page.waitFor(5000);
+    // //await page.waitForNetworkIdle();
+    //
+    // // Get cookies
+    // const cookies = await page.cookies();
+    //
+    // // Use cookies in another tab or browser
+    // const page2 = await browser.newPage();
+    // await page2.setCookie(...cookies);
+    //
+    // // Open the page as a logged-in user
+    // await page2.goto('https://www.lehrerbuero.de/dokumente.html');
+    // await page2.waitForNetworkIdle();
 
-    // Login
-    await page.type('#user', '123');
-    await page.type('#pass', '345');
-    await page.click('button[name="submit"]');
-    await page.waitForNetworkIdle();
+    const page2 = await browser.newPage();
+    await page2.goto('http://127.0.0.1:3000');
 
-    // Get cookies
-    const cookies = await page.cookies();
 
-    // Use cookies in another tab or browser
-    //const page2 = await browser.newPage();
-    //await page2.setCookie(cookies);
+    const dimensions = await page2.evaluate(() => {
+        return {
+            width: document.documentElement.clientWidth,
+            height: document.documentElement.clientHeight,
+            deviceScaleFactor: window.devicePixelRatio,
+        };
+    });
 
-    // Open the page as a logged-in user
-    //await page2.goto('https://www.lehrerbuero.de/mydocuments');
+    console.log('Dimensions:', dimensions);
+
+    const files = await page2.evaluate(() => {
+        let elements = Array.from(document.querySelectorAll('table.file-listing tbody tr.filetype-file td.file-title a'));
+        let href = elements.map(element => {
+            return element.href;
+        });
+        return href;
+    });
+
+    console.log("Number of Files: " + files.length);
+
+    for(let i = 0; i < files.length; i++) {
+        console.log(files[i]);
+    }
 
     //await browser.close();
 
