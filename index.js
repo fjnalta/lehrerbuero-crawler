6a0b7b8722e87ee8da146876a4c41c742e7957ad
Philipp Minges <fjnalta@gmail.com>
2021-10-31 22:10:37 +0100

    implement file download

--- index.js
+++ index.js
@@ -1,30 +1,82 @@
-const Apify = require('apify');
+const puppeteer = require('puppeteer');
+const path = require('path');
+const downloadPath = path.resolve('./download');
 
-const { log } = Apify.utils;
+const loginHandler = new (require('./lib/loginHandler'))();
+const myDocumentsHandler = new (require('./lib/myDocumentsHandler'))();
+const categoryHandler = new (require('./lib/categoryHandler'))();
 
-const config = require('./config');
+async function crawler() {
 
-Apify.main(async () => {
-    const browser = await Apify.launchPuppeteer();
+    // setup puppeteer
+    const browser = await puppeteer.launch({ headless : false });
 
-    // const page = await browser.newPage();
+    // login to account and get cookies
+    let cookies = await loginHandler.login(browser);
+
+    // open my Documents page
+    let myDocumentsPage = await myDocumentsHandler.openPage(browser, cookies);
+
+    // get files
+    let files = await myDocumentsHandler.getItems(myDocumentsPage);
+
+    // download files
+    for (let i = 0; i < files.length; i++) {
+        await myDocumentsHandler.downloadItem(files[i], downloadPath, cookies);
+    }
+
+    // generate URL's from categories
+    //let categories = categoryHandler.generateCategoryURLs();
+    //log.info("fetched the following category URL's");
+    //for(let i = 0; i < categories.length; i++) {
+    //    log.info(i+1 + ". " + categories[i]);
+    //}
+
+    // open categories in new tab
+    // for(let i = 0; i < categories.length; i++) {
+    //     // open new tab
+    //     let categoryPage = await browser.newPage();
+    //     // setup cookies
+    //     categoryPage.setCookie(...cookies);
+    //     // open current category
+    //     await categoryPage.goto(categories[i]);
+    //     await categoryPage.waitForNetworkIdle();
     //
-    // await page.goto('https://www.lehrerbuero.de/anmelden.html');
-    // await page.waitForNetworkIdle();
+    //     // TODO - check if last page is reached
+    //     // get all items from current page
+    //     let itemsFromPage = categoryHandler.getItemsFromPage(categoryPage);
     //
-    // // Accept cookies
-    // await page.click('#uc-btn-accept-banner');
+    //     for(let i = 0; i < itemsFromPage.length; i++) {
+    //         let currentItem = itemsFromPage[i];
+    //         // move current item to my documents
     //
-    // // Login
-    // await page.type('#user', config.username);
-    // await page.type('#pass', config.password);
-    // await page.click('button[name="submit"]');
     //
-    // await page.waitFor(5000);
+    //         // open new tab for my documents
+    //         let myDocumentsPage = await browser.newPage();
+    //         myDocumentsPage.setCookie(...cookies);
+    //         await myDocumentsPage.goto('https://www.lehrerbuero.de/dokumente.html');
+    //         await myDocumentsPage.waitForNetworkIdle();
+    //
+    //         // download current item
+    //
+    //         // remove item from my documents
+    //
+    //         // close my documents tab
+    //         await myDocumentsPage.close();
+    //     }
+    //
+    //     // check if this was the last page
+    //     if(categoryHandler.isLastPage() === false) {
+    //
+    //     }
+    //     // close category tab
+    //     await categoryPage.close();
+    // }
+
+
     // //await page.waitForNetworkIdle();
     //
-    // // Get cookies
-    // const cookies = await page.cookies();
+
     //
     // // Use cookies in another tab or browser
     // const page2 = await browser.newPage();
@@ -34,35 +86,38 @@ Apify.main(async () => {
     // await page2.goto('https://www.lehrerbuero.de/dokumente.html');
     // await page2.waitForNetworkIdle();
 
-    const page2 = await browser.newPage();
-    await page2.goto('http://127.0.0.1:3000');
-
-
-    const dimensions = await page2.evaluate(() => {
-        return {
-            width: document.documentElement.clientWidth,
-            height: document.documentElement.clientHeight,
-            deviceScaleFactor: window.devicePixelRatio,
-        };
-    });
-
-    console.log('Dimensions:', dimensions);
-
-    const files = await page2.evaluate(() => {
-        let elements = Array.from(document.querySelectorAll('table.file-listing tbody tr.filetype-file td.file-title a'));
-        let href = elements.map(element => {
-            return element.href;
-        });
-        return href;
-    });
-
-    console.log("Number of Files: " + files.length);
-
-    for(let i = 0; i < files.length; i++) {
-        console.log(files[i]);
-    }
+    // const page2 = await browser.newPage();
+    // await page2.goto('http://127.0.0.1:3000');
+    //
+    //
+    // const dimensions = await page2.evaluate(() => {
+    //     return {
+    //         width: document.documentElement.clientWidth,
+    //         height: document.documentElement.clientHeight,
+    //         deviceScaleFactor: window.devicePixelRatio,
+    //     };
+    // });
+    //
+    // console.log('Dimensions:', dimensions);
+    //
+    // const files = await page2.evaluate(() => {
+    //     let elements = Array.from(document.querySelectorAll('table.file-listing tbody tr.filetype-file td.file-title a'));
+    //     let href = elements.map(element => {
+    //         return element.href;
+    //     });
+    //     return href;
+    // });
+    //
+    // console.log("Number of Files: " + files.length);
+    //
+    // for(let i = 0; i < files.length; i++) {
+    //     console.log(files[i]);
+    // }
 
     //await browser.close();
 
     //log.info('Done.');
-});
+//});
+}
+
+crawler();
